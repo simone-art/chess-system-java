fe74da1dc9a09937353fd146e50e2d7796684504
simone-art <simoneperiodista3@gmail.com>
2023-05-09 18:27:50 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,11 +29,18 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	//Método que recebe as coordenadas do xadrez
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		//Converte o xadrez para a posição de matriz
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		
+	}
+	
 	//Método que vai imprimir  e inicializar no tabuleiro as peças Torre e King
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 	
 	
