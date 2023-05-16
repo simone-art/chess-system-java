296244c06ca696264e6d14130e8e92cdf7c77487
simone-art <simoneperiodista3@gmail.com>
2023-05-16 19:23:17 -0300

    Implementing possible moves of King

--- src/chess/piece/King.java
+++ src/chess/piece/King.java
@@ -1,6 +1,7 @@
 package chess.piece;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,13 +17,70 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
-
+   
+	//Método que valida se a peça pode-se mover
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		//a casa está vazia ou tem uma peça adversária
+		return p == null || p.getColor() != getColor();
+		
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		//Criada matriz de booleanos com a dimensão do tabuleiro
 		//Por padrão todas as posições da matriz começam com false
 		boolean[][] mat = new boolean [getBoard().getRows()] [getBoard().getColumns()];
+		
+		Position p = new Position(0,0);
+		
+		//above - acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if(getBoard().positionExits(p)&& canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//below - abaixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//left - esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//rigth - direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//diagonal noroeste nw
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//diagonal nordeste ne
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//diagonal suleste se
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//diagonal suldeste sd
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if(getBoard().positionExits(p)&& canMove(p)) {
+		mat[p.getRow()][p.getColumn()] = true;
+				}
 		return mat;
 	}
 
