7c1738994e57bb73b1f9bcb77d1c1885422e0257
simone-art <simoneperiodista3@gmail.com>
2023-06-19 16:43:14 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -139,7 +138,8 @@ public class ChessMatch {
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && 
 				!type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			// retorna a peça que está lá
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
